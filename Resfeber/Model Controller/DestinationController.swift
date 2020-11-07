dd74f00d15252c93e9a23a4e051fa7994fbce414
Josh <jrtkski@icloud.com>
2020-11-07 10:20:42 -0700

    Created DestinationController
    Created DestinationController class for CRUD methods.
    
    Set up basic scaffolding for methods to create, read, update, and delete a destination.

---
+++ Resfeber/Model Controller/DestinationController.swift
@@ -0,0 +1,28 @@
+//
+//  DestinationController.swift
+//  Resfeber
+//
+//  Created by Joshua Rutkowski on 11/7/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+class DestinationController {
+    
+    static func createDestination(destination: Destination) {
+        
+    }
+    
+    static func readDestinations() {
+        
+    }
+    
+    static func updateDestination(destination: Destination) {
+        
+    }
+    
+    static func deleteDestinatino(destination: Destination) {
+        
+    }
+}
