d2b950a08094eaa91727d8de10b650d201bedf28
Josh <jrtkski@icloud.com>
2020-11-06 20:14:29 -0700

    Created UIColor Extension
    Create UIColor extension for custom Resfeber color sets. Implemented UIColor.Resfeber struct to store values.

---
+++ Resfeber/Helpers/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  Resfeber
+//
+//  Created by Joshua Rutkowski on 11/6/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  struct Resfeber {
+    static var red: UIColor  { return UIColor(named: "ResfeberRed")! }
+    static var deleteRed: UIColor  { return UIColor(named: "ResfeberDeleteRed")! }
+    static var blue: UIColor  { return UIColor(named: "ResfeberBlue")! }
+    static var yellow: UIColor  { return UIColor(named: "ResfeberYellow")! }
+    static var green: UIColor  { return UIColor(named: "ResfeberGreen")! }
+    static var dark: UIColor  { return UIColor(named: "ResfeberDark")! }
+    static var light: UIColor  { return UIColor(named: "ResfeberLight")! }
+    static var background: UIColor  { return UIColor(named: "ResfeberBackground")! }
+  }
+}
