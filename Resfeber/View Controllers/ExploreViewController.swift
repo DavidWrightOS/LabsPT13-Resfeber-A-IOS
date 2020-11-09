4afe961b6e33d48995591ed44f1f73fbec60a43b
David Wright <davidtwright89@gmail.com>
2020-11-08 21:46:57 -0500

    Added an "Explore" view controller which will display destinations and allow the user to search

---
+++ Resfeber/View Controllers/ExploreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreViewController.swift
+//  Resfeber
+//
+//  Created by David Wright on 11/8/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Helpers
+    
+    
+}
