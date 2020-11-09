ce6d09b6e5ea79599aad1465a6168e22031c9c39
David Wright <davidtwright89@gmail.com>
2020-11-09 01:40:14 -0500

    Added a title and a profile button to the top of the explorer view controller

--- Resfeber/View Controllers/ExploreViewController.swift
+++ Resfeber/View Controllers/ExploreViewController.swift
@@ -12,18 +12,57 @@ class ExploreViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        label.textColor = .label
+        label.text = "Explore"
+        return label
+    }()
     
+    private let profileButton: UIButton = {
+        let button = UIButton(type: .system)
+        let buttonDiameter: CGFloat = 32
+        button.setDimensions(height: buttonDiameter, width: buttonDiameter)
+        button.layer.cornerRadius = buttonDiameter / 2
+        button.layer.masksToBounds = true
+        button.contentMode = .scaleAspectFill
+        button.backgroundColor = .systemGray3
+        button.addTarget(self, action: #selector(profileImageTapped), for: .touchUpInside)
+        
+        let image = UIImage(systemName: "person.fill")!.withTintColor(.systemGray6, renderingMode: .alwaysOriginal)
+        button.setImage(image, for: .normal)
+        
+        return button
+    }()
+        
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureViews()
     }
     
     // MARK: - Selectors
     
+    @objc fileprivate func profileImageTapped() {
+        print("DEBUG: profileImageTapped..")
+    }
     
     // MARK: - Helpers
     
-    
+    fileprivate func configureViews() {
+        view.backgroundColor = UIColor.Resfeber.background
+        navigationController?.navigationBar.isHidden = true
+        
+        // Configure Title Label
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
+                          right: view.rightAnchor, paddingTop: 4, paddingLeft: 20, paddingRight: 20)
+        
+        // Configure Profile Button
+        view.addSubview(profileButton)
+        profileButton.anchor(right: view.rightAnchor, paddingRight: 20)
+        profileButton.centerY(inView: titleLabel)
+    }
 }
