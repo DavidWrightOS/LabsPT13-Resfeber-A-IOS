58d3966336c90ef06cbbefd6959e7d17b68ea288
David Wright <davidtwright89@gmail.com>
2020-11-12 01:58:07 -0500

    Moved the tab bar setup from the SceneDelegate to the MainTabBarController so the configuration can be setup locally

--- Resfeber/View Controllers/MainTabBarController.swift
+++ Resfeber/View Controllers/MainTabBarController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+        
+    // MARK: - Lifecycle
  
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,37 @@ class MainTabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         UITabBar.appearance().tintColor = UIColor.Resfeber.red
+        configureViewControllers()
+    }
+
+    // MARK: - Helpers
+    
+    private func configureViewControllers() {
+        let symbolConfig = UIImage.SymbolConfiguration(weight: .bold)
+        
+        let exploreVC = ExploreViewController()
+        let exploreTabImage = UIImage(systemName: "house", withConfiguration: symbolConfig)
+        let exploreNav = tabNavigationController(rootViewController: exploreVC,
+                                                 tabImage: exploreTabImage)
+        
+        let favoritesVC = FavoritesViewController()
+        let favoritesTabImage = UIImage(systemName: "heart", withConfiguration: symbolConfig)
+        let favoritesNav = tabNavigationController(rootViewController: favoritesVC,
+                                                   tabImage: favoritesTabImage)
+        
+        let itineraryVC = ItineraryViewController()
+        let itineraryTabImage = UIImage(systemName: "briefcase", withConfiguration: symbolConfig)
+        let itineraryNav = tabNavigationController(rootViewController: itineraryVC,
+                                                   tabImage: itineraryTabImage)
+        
+        viewControllers = [exploreNav, favoritesNav, itineraryNav]
+    }
+    
+    private func tabNavigationController(rootViewController: UIViewController, tabImage: UIImage?) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.image = tabImage
+        navController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navController.navigationBar.shadowImage = UIImage()
+        return navController
     }
 }
