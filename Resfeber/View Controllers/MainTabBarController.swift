6b0c99af405a64ea5b31db4295ca382b6a99460f
Josh <jrtkski@icloud.com>
2020-11-11 19:49:18 -0700

    Set up tabBarItem tint Color
    Set UITabBar tint Color to UIColor.Resfeber.red to align with UX guidelines

--- Resfeber/View Controllers/MainTabBarController.swift
+++ Resfeber/View Controllers/MainTabBarController.swift
@@ -9,22 +9,13 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UITabBar.appearance().tintColor = UIColor.Resfeber.red
     }
-    */
-
 }
