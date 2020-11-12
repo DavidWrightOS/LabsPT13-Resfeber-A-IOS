877443a23b5c4ea17c98f17f31da881c5d602eee
Josh <jrtkski@icloud.com>
2020-11-11 19:12:10 -0700

    Setup TabBarController and View Controllers
    Initial setup for MainTabBarController and scaffolding for ItineraryViewController and FavoritesViewController

---
+++ Resfeber/View Controllers/ItineraryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItineraryViewController.swift
+//  Resfeber
+//
+//  Created by Joshua Rutkowski on 11/11/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ItineraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
