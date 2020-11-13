b866e1eb40a1f94505d1b6a5997864878bdc24c6
David Wright <davidtwright89@gmail.com>
2020-11-13 03:13:05 -0500

    Copied the code from the favorites view controller into the itinerary controller to mimic the UI/setup

--- Resfeber/View Controllers/ItineraryViewController.swift
+++ Resfeber/View Controllers/ItineraryViewController.swift
@@ -10,9 +10,89 @@ import UIKit
 
 class ItineraryViewController: UIViewController {
 
+    // MARK: - Properties
+
+    fileprivate var collectionView: UICollectionView!
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configureViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+
+    // MARK: - Helpers
+
+    fileprivate func configureViews() {
+        view.backgroundColor = UIColor.Resfeber.background
         
+        // Configure Collection View
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.Resfeber.background
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(DestinationCell.self, forCellWithReuseIdentifier: DestinationCell.reuseIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                              left: view.leftAnchor,
+                              bottom: view.bottomAnchor,
+                              right: view.rightAnchor,
+                              paddingTop: 12,
+                              paddingLeft: 20,
+                              paddingRight: 20)
+        
+        // Load Data
+        DestinationController.readDestinations()
+        collectionView.reloadData()
+    }
+}
+
+// MARK: - Collection View Layout
+
+extension ItineraryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout _: UICollectionViewLayout, sizeForItemAt _: IndexPath) -> CGSize {
+        let width = collectionView.frame.size.width
+        let xInsets: CGFloat = 16
+        let cellWidth = width - (xInsets * 2)
+        let cellHeight: CGFloat = cellWidth * 0.65
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+}
+
+// MARK: - Collection View Data Source
+
+extension ItineraryViewController: UICollectionViewDataSource {
+    func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
+        DestinationData.favoriteDestinations.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DestinationCell.reuseIdentifier, for: indexPath) as! DestinationCell
+        guard indexPath.row < DestinationData.favoriteDestinations.count else { return DestinationCell() }
+
+        cell.destination = DestinationData.favoriteDestinations[indexPath.row]
+
+        return cell
+    }
+}
+
+// MARK: - Collection View Delegate
+
+extension ItineraryViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard indexPath.row < DestinationData.favoriteDestinations.count else { return }
+        let destination = DestinationData.favoriteDestinations[indexPath.row]
+        print("DEBUG: Tapped destination: \(destination.name)..")
     }
 }
