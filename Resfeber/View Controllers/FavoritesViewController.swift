e6785133fbce36754e056598de891e572106eff1
David Wright <davidtwright89@gmail.com>
2020-11-13 02:54:54 -0500

    Setup the collection view layout

--- Resfeber/View Controllers/FavoritesViewController.swift
+++ Resfeber/View Controllers/FavoritesViewController.swift
@@ -32,6 +32,8 @@ class FavoritesViewController: UIViewController {
         collectionView.backgroundColor = UIColor.Resfeber.background
         collectionView.showsVerticalScrollIndicator = false
         collectionView.register(DestinationCell.self, forCellWithReuseIdentifier: DestinationCell.reuseIdentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
         view.addSubview(collectionView)
         collectionView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
                               left: view.leftAnchor,
@@ -41,5 +43,35 @@ class FavoritesViewController: UIViewController {
                               paddingLeft: 20,
                               paddingRight: 20)
         
+    }
+}
+
+// MARK: - Collection View Layout
+
+extension FavoritesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout _: UICollectionViewLayout, sizeForItemAt _: IndexPath) -> CGSize {
+        let width = collectionView.frame.size.width
+        let xInsets: CGFloat = 16
+        let cellWidth = width - (xInsets * 2)
+        let cellHeight: CGFloat = cellWidth * 0.65
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+}
+
+// MARK: - Collection View Data Source
+
+extension FavoritesViewController: UICollectionViewDataSource {
+    func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DestinationCell.reuseIdentifier, for: indexPath) as! DestinationCell
+
+        return cell
     }
 }
