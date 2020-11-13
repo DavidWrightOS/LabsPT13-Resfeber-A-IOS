61d19692637726aa4e3475e57c0abea1cf0bb33b
David Wright <davidtwright89@gmail.com>
2020-11-13 02:50:46 -0500

    Added a collectionView to the favorites controller

--- Resfeber/View Controllers/FavoritesViewController.swift
+++ Resfeber/View Controllers/FavoritesViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    fileprivate var collectionView: UICollectionView!
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
+    }
+    
+    // MARK: - Helpers
+
+    fileprivate func configureViews() {
+        view.backgroundColor = UIColor.Resfeber.background
+        
+        // Configure Collection View
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.Resfeber.background
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(DestinationCell.self, forCellWithReuseIdentifier: DestinationCell.reuseIdentifier)
+        view.addSubview(collectionView)
+        collectionView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                              left: view.leftAnchor,
+                              bottom: view.bottomAnchor,
+                              right: view.rightAnchor,
+                              paddingTop: 12,
+                              paddingLeft: 20,
+                              paddingRight: 20)
         
     }
 }
