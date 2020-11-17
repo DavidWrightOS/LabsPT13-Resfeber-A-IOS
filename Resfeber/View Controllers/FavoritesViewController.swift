fa35fb641f173e82fb345fd2faf4d7f4f71071d6
David Wright <davidtwright89@gmail.com>
2020-11-17 01:08:14 -0500

    Updated each of the three main view controllers to store an identical instance of DestinationController, which is set via dependency injection in the MainTabBarController

--- Resfeber/View Controllers/FavoritesViewController.swift
+++ Resfeber/View Controllers/FavoritesViewController.swift
@@ -12,10 +12,20 @@ class FavoritesViewController: UIViewController {
     
     // MARK: - Properties
 
+    fileprivate let destinationController: DestinationController
     fileprivate var collectionView: UICollectionView!
 
     // MARK: - Lifecycle
-
+    
+    init(destinationController: DestinationController) {
+        self.destinationController = destinationController
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
@@ -49,7 +59,7 @@ class FavoritesViewController: UIViewController {
                               paddingRight: 20)
         
         // Load Data
-        DestinationController.readDestinations()
+        destinationController.readDestinations()
         collectionView.reloadData()
     }
 }
@@ -74,14 +84,14 @@ extension FavoritesViewController: UICollectionViewDelegateFlowLayout {
 
 extension FavoritesViewController: UICollectionViewDataSource {
     func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
-        DestinationData.favoriteDestinations.count
+        destinationController.favoriteDestinations.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DestinationCell.reuseIdentifier, for: indexPath) as! DestinationCell
-        guard indexPath.row < DestinationData.favoriteDestinations.count else { return DestinationCell() }
+        guard indexPath.row < destinationController.favoriteDestinations.count else { return DestinationCell() }
 
-        cell.destination = DestinationData.favoriteDestinations[indexPath.row]
+        cell.destination = destinationController.favoriteDestinations[indexPath.row]
 
         return cell
     }
@@ -91,8 +101,8 @@ extension FavoritesViewController: UICollectionViewDataSource {
 
 extension FavoritesViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard indexPath.row < DestinationData.favoriteDestinations.count else { return }
-        let destination = DestinationData.favoriteDestinations[indexPath.row]
+        guard indexPath.row < destinationController.favoriteDestinations.count else { return }
+        let destination = destinationController.favoriteDestinations[indexPath.row]
         print("DEBUG: Tapped destination: \(destination.name)..")
     }
 }
--- Resfeber/View Controllers/ItineraryViewController.swift
+++ Resfeber/View Controllers/ItineraryViewController.swift
@@ -12,10 +12,20 @@ class ItineraryViewController: UIViewController {
 
     // MARK: - Properties
 
+    fileprivate let destinationController: DestinationController
     fileprivate var collectionView: UICollectionView!
 
     // MARK: - Lifecycle
 
+    init(destinationController: DestinationController) {
+        self.destinationController = destinationController
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
@@ -49,7 +59,7 @@ class ItineraryViewController: UIViewController {
                               paddingRight: 20)
         
         // Load Data
-        DestinationController.readDestinations()
+        destinationController.readDestinations()
         collectionView.reloadData()
     }
 }
@@ -74,14 +84,14 @@ extension ItineraryViewController: UICollectionViewDelegateFlowLayout {
 
 extension ItineraryViewController: UICollectionViewDataSource {
     func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
-        DestinationData.itineraryDestinations.count
+        destinationController.itineraryDestinations.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DestinationCell.reuseIdentifier, for: indexPath) as! DestinationCell
-        guard indexPath.row < DestinationData.itineraryDestinations.count else { return DestinationCell() }
+        guard indexPath.row < destinationController.itineraryDestinations.count else { return DestinationCell() }
 
-        cell.destination = DestinationData.itineraryDestinations[indexPath.row]
+        cell.destination = destinationController.itineraryDestinations[indexPath.row]
 
         return cell
     }
@@ -91,8 +101,8 @@ extension ItineraryViewController: UICollectionViewDataSource {
 
 extension ItineraryViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard indexPath.row < DestinationData.itineraryDestinations.count else { return }
-        let destination = DestinationData.itineraryDestinations[indexPath.row]
+        guard indexPath.row < destinationController.itineraryDestinations.count else { return }
+        let destination = destinationController.itineraryDestinations[indexPath.row]
         print("DEBUG: Tapped destination: \(destination.name)..")
     }
 }
