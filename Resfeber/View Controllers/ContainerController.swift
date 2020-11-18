cacee4c2c1694c2bb29dbd7ba6a358717e37e34b
David Wright <davidtwright89@gmail.com>
2020-11-18 08:46:30 -0500

    Added the animations to the container controller

--- Resfeber/View Controllers/ContainerController.swift
+++ Resfeber/View Controllers/ContainerController.swift
@@ -14,6 +14,7 @@ class ContainerController: UIViewController {
     
     fileprivate var sideMenuController: SideMenuController!
     fileprivate var mainTabBarController: MainTabBarController!
+    fileprivate var isExpanded = false
     
     // MARK: - Lifecycle
     
@@ -45,6 +46,66 @@ class ContainerController: UIViewController {
             sideMenuController.didMove(toParent: self)
             sideMenuController.view.frame.origin.x = -view.frame.width
         }
+    }
+    
+    fileprivate func didSelectMenuOption(menuOption: MenuOption) {
+        switch menuOption {
+        case .EditProfile:
+            let nav = UINavigationController(rootViewController: ProfileViewController())
+            present(nav, animated: true, completion: nil)
+        case .LogOut:
+            break //TODO: Log user out
+        }
+    }
+    
+    // MARK: - Selectors
+    
+    @objc fileprivate func dismissMenu() {
+        isExpanded = false
+        animateSideMenu(shouldExpand: isExpanded, menuOption: nil)
+    }
+    
+    // MARK: - Animation
+    
+    fileprivate func animateSideMenu(shouldExpand: Bool, menuOption: MenuOption?) {
+        
+        if shouldExpand {
+            // Show side menu
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 1.0,
+                           initialSpringVelocity: 5.0,
+                           options: .curveEaseOut,
+                           animations: {
+                            self.sideMenuController.view.frame.origin.x = 0
+                            self.mainTabBarController.view.frame.origin.x = self.view.frame.width - 80
+                           }, completion: nil)
+            
+        } else {
+            // Hide side menu
+            UIView.animate(withDuration: 0.15, delay: 0, animations: {
+                self.sideMenuController.view.frame.origin.x = -self.view.frame.width
+                self.mainTabBarController.view.frame.origin.x = 0
+            }) { (_) in
+                guard let menuOption = menuOption else { return }
+                self.didSelectMenuOption(menuOption: menuOption)
+            }
+        }
+        
+        animateStatusBar()
+    }
+    
+    fileprivate func animateStatusBar() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        
+                        self.setNeedsStatusBarAppearanceUpdate()
+                        
+                       }, completion: nil)
     }
 }
 
