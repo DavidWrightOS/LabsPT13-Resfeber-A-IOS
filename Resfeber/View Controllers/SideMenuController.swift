db7520ce893e52e831c6707abf9f63bf17e30773
David Wright <davidtwright89@gmail.com>
2020-11-18 08:16:45 -0500

    Setup the side menu's data source

--- Resfeber/View Controllers/SideMenuController.swift
+++ Resfeber/View Controllers/SideMenuController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-enum MenuOption: Int, CustomStringConvertible {
+private let reuseIdentifer = "MenuOptionCell"
+
+enum MenuOption: Int, CustomStringConvertible, CaseIterable {
     
     case EditProfile
     case LogOut
@@ -33,11 +35,49 @@ class SideMenuController: UIViewController {
     // MARK: - Properties
     
     var profile: Profile = ProfileController.shared.profile
+    var tableView: UITableView!
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.Resfeber.red
+        configureTableView()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureTableView() {
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(SideMenuCell.self, forCellReuseIdentifier: reuseIdentifer)
+        tableView.backgroundColor = UIColor.Resfeber.red
+        tableView.separatorStyle = .none
+        tableView.isScrollEnabled = false
+        tableView.rowHeight = 60
+        
+        view.addSubview(tableView)
+        tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+}
+
+// MARK: - Table View Delegate & DataSource
+
+
+extension SideMenuController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MenuOption.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath) as! SideMenuCell
+        
+        let menuOption = MenuOption(rawValue: indexPath.row)
+        cell.descriptionLabel.text = menuOption?.description
+        cell.iconImageView.image = menuOption?.image
+        
+        return cell
     }
 }
