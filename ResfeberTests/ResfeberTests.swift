a2ecf2006e5516e0251edc68491f6cf25fb0c5b5
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-12-04 08:48:32 -0500

    Added Core Data stack class. Added unit testing target.

---
+++ ResfeberTests/ResfeberTests.swift
@@ -0,0 +1,34 @@
+//
+//  ResfeberTests.swift
+//  ResfeberTests
+//
+//  Created by Joshua Rutkowski on 12/4/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import XCTest
+import Resfeber
+
+class ResfeberTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
