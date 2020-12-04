c75251ba922e5b06f72a1af4531beba33464f524
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-12-04 14:39:13 -0500

    Updated model to include Event.

---
+++ Resfeber/Model/Core Data/Event+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Event+CoreDataClass.swift
+//  Resfeber
+//
+//  Created by Joshua Rutkowski on 12/4/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
--- Resfeber/Model/Core Data/Trip+CoreDataProperties.swift
+++ Resfeber/Model/Core Data/Trip+CoreDataProperties.swift
@@ -18,9 +18,27 @@ extension Trip {
     }
 
     @NSManaged public var endDate: Date?
-    @NSManaged public var startDate: Date?
     @NSManaged public var image: Data?
     @NSManaged public var name: String?
+    @NSManaged public var startDate: Date?
+    @NSManaged public var events: NSSet?
+
+}
+
+// MARK: Generated accessors for events
+extension Trip {
+
+    @objc(addEventsObject:)
+    @NSManaged public func addToEvents(_ value: Event)
+
+    @objc(removeEventsObject:)
+    @NSManaged public func removeFromEvents(_ value: Event)
+
+    @objc(addEvents:)
+    @NSManaged public func addToEvents(_ values: NSSet)
+
+    @objc(removeEvents:)
+    @NSManaged public func removeFromEvents(_ values: NSSet)
 
 }
 
