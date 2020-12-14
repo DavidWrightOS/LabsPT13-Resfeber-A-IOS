2d3a167a863a5219ba76d6ae476fe012bccc87d1
David Wright <davidtwright89@gmail.com>
2020-12-14 08:41:44 -0500

    Added CLLocation and CLPlacemark extensions for generating an address string from a latitude and longitude

---
+++ Resfeber/Helpers/CLPlacemark+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  CLPlacemark+Extension.swift
+//  Resfeber
+//
+//  Created by David Wright on 12/14/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import CoreLocation
+
+extension CLPlacemark {
+    var address: String? {
+        guard let subThoroughfare = subThoroughfare,
+              let thoroughfare = thoroughfare,
+              let locality = locality,
+              let adminArea = administrativeArea else { return nil }
+        
+        return "\(subThoroughfare) \(thoroughfare), \(locality), \(adminArea) "
+    }
+}
