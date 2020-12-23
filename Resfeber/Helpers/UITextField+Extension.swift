8d9ef5d883138857f9588fda00c6ab0d43df767a
David Wright <davidtwright89@gmail.com>
2020-12-23 12:31:40 -0500

    Added a textView extension for configuring a pickerView as its inputView

--- Resfeber/Helpers/UITextField+Extension.swift
+++ Resfeber/Helpers/UITextField+Extension.swift
@@ -33,6 +33,30 @@ extension UITextField {
         return datePicker
     }
     
+    @discardableResult
+    func setInputViewCategoryPicker<T: UIPickerViewDataSource & UIPickerViewDelegate>(target: T, selector: Selector) -> UIPickerView {
+        // Create a UIPickerView object and assign to inputView
+        let screenWidth = UIScreen.main.bounds.width
+        let picker = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
+        picker.dataSource = target
+        picker.delegate = target
+        picker.sizeToFit()
+        picker.tintColor = RFColor.red
+        self.inputView = picker
+        
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel))
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
+        cancel.tintColor = RFColor.red
+        barButton.tintColor = RFColor.red
+        toolBar.setItems([cancel, flexible, barButton], animated: false)
+        self.inputAccessoryView = toolBar
+        
+        return picker
+    }
+    
     @objc func tapCancel() {
         self.resignFirstResponder()
     }
