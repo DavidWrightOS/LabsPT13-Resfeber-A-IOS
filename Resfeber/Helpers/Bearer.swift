6fbb5dd6c8d5939a1ec8fb41a32d09fe2bb5d60a
David Wright <davidtwright89@gmail.com>
2021-01-31 04:20:50 -0500

    Added some networking helper models

---
+++ Resfeber/Helpers/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Resfeber
+//
+//  Created by David Wright on 1/21/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Resfeber/Helpers/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Resfeber
+//
+//  Created by David Wright on 1/21/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
